ee91909d4dd26cb1403eda733e2277a9111ebc81
Isabelino Rolando Lopez Roman <isabelino.rolando@gmail.com>
2022-12-15 16:14:40 +0100

    ultimos cambios crud completo

--- springweb-mvc/src/main/java/com/formacion/springweb/controller/EmpleadoController.java
+++ springweb-mvc/src/main/java/com/formacion/springweb/controller/EmpleadoController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.formacion.springweb.entity.Empleado;
 import com.formacion.springweb.service.EmpleadoService;
 
 @Controller
@@ -20,8 +24,49 @@ public class EmpleadoController {
 	}
 	
 	@GetMapping("empleados/crear")
-	public String newEmpleado() {
+	public String newEmpleado(Model modelo) {
+		Empleado empleado = new Empleado();
+		modelo.addAttribute("empleado", empleado);
 		return "crear_empleado";
 	}
+	@PostMapping("empleado")
+	public String addEmpleado(@ModelAttribute("empleado") Empleado empleado) {
+		servicio.guardarEmpleado(empleado);
+		return "redirect:/empleados";
+	}
+	@GetMapping("empleado/editar/{id}")
+	public String editEmpleado(@PathVariable long id, Model modelo) {
+		modelo.addAttribute("empleado", servicio.obtenerEmpleado(id) );
+		return "editar_empleado";
+	}
+	
+	@PostMapping("empleado/update/{id}")
+	public String updateEmpleado(@PathVariable long id,@ModelAttribute("empleado") Empleado empleado) {
+		
+		
+		empleado.setId(servicio.obtenerEmpleado(id).getId());
+		
+		
+		/*Empleado empleadoExistente = servicio.obtenerEmpleado(id);
+		empleadoExistente.setId(id);
+		empleadoExistente.setNombre(empleado.getNombre());
+		empleadoExistente.setApellido(empleado.getApellido());
+		empleadoExistente.setEmail(empleado.getEmail());
+		empleadoExistente.setTelefono(empleado.getTelefono());*/
+		
+		servicio.guardarEmpleado( empleado);
+		
+		return "redirect:/empleados";
+	}
+	
+	@GetMapping("/empleado/borrar/{id}")
+	public String eliminarEmpleado(@PathVariable Long id) {
+		servicio.eliminarEmpleado(id);
+		return "redirect:/empleados"; 
+	}
+	
+	
+	
+	
 
 }
